d88b118c1231c531ab3f8791e2951c5affd7e7b5
sigdelbjay <sigdelbjay@gmail.com>
2020-12-01 08:20:51 +1100

    Adds BurgerBuilder class component, Burger functional component, dynamically builds/displays burger based on set burger ingredients

---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import buildControl from './BuildControl/BuildControl';
+
+const buildControls = (props) => {
+  return (
+    <div></div>
+  );
+}
+
+export default buildControls;
---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux'
+import Burger from '../../components/Burger/Burger';
+
+class BurgerBuilder extends Component {
+
+  state = {
+    ingredients: {
+      meat: 0,
+      cheese: 0,
+      salad: 0,
+      bacon:0
+    }
+  }
+
+  render() {
+    return (
+      <Aux>
+        <Burger ingredients={this.state.ingredients}/>
+        <div>Build Controls</div>
+      </Aux>
+    )
+  }
+}
+
+export default BurgerBuilder;
